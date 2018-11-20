027446e2e9a45c1a7a63ac2bdcdd2904ce85d7be
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-20 10:25:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+   get '/newteam' do
+     erb :newteam
+   end
+   post '/newteam' do
+     @values=params
+     erb :team
+   end
 
+   get '/newteam' do
+     erb :newteam
+   end
 
 end
